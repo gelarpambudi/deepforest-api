5211ea139fa0498d854320aafe55146cc03f6cdd
Gelar Pambudi <gelargelur@gmail.com>
2021-01-04 14:12:31 +0700

    add center coordinate dataframe

--- predict_deepforest.py
+++ predict_deepforest.py
@@ -21,15 +21,17 @@ def predict(input_image, model):
                         iou_threshold=0.2, 
                         patch_size=700
                     )
+
+    bounding_boxes["xcenter"] = get_x_center(bounding_boxes["xmin"], bounding_boxes["xmax"])
+    bounding_boxes["ycenter"] = get_y_center(bounding_boxes["ymin"], bounding_boxes["ymax"])
+
     return bounding_boxes.to_json(orient='index')
 
-def get_center_coordinate(xmin, xmax, ymin, ymax):
-    xcenter = (xmin + xmax) * 0.5
-    y_center = (ymin + ymax) * 0.5
-    return x_center, y_center
+def get_x_center(xmin, xmax):
+    return (xmin + xmax) * 0.5
 
-def add_to_df(x_center, y_center):
-    return ...
+def get_y_center(ymin, ymax):
+    return (ymin + ymax) * 0.5
 
 def save_image(img_file):
     filename = secure_filename(img_file.filename)
